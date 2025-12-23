7c60c056fb7af0af9fc91315ac869bfd24e696dd
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2025-12-23 13:53:27 +0000

    Server setup

--- server.js
+++ server.js
@@ -1,1 +1,22 @@
-ÿþ
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get('/', (req, res) => {
+  res.send('MPFL API running');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
