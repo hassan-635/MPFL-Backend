e9bbf48c94f301bb1d3ec3147f49fb952405af00
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2025-12-25 19:15:11 +0000

    cloudinary configuration with middleware

---
+++ middleware/uploadMiddleware.js
@@ -0,0 +1,21 @@
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUINARY_CLOUD_NAME,
+  cloud_api_key: process.env.CLOUDINARY_API_KEY,
+  cloud_api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "mpfl_proofs",
+    resource_type: "auto",
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
