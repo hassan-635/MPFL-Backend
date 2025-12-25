590c7d8c0ed6c3b3c28c776c32eb5a77fde74be2
Hassan Ali Abrar <hassanalimughal635@gmail.com>
2025-12-25 07:37:06 +0000

    added routes for authentication

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,47 +1,47 @@
-import User from '../models/User';
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const user = require('../models/User')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-
-export const register = async (req, res) => {
-    try {
-        const {name, email, password} = req.body;
-        const userExists = await User.findOne({email});
-        if(userExists){
-            return res.status(400).json({ message: 'User already Exists'});
-        }
-
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        const user = await User.create({
-            name,
-            email,
-            password: hashedPassword
-        })
-        res.status(200).json({message: 'User Registered Successfully!!!'});
-    } catch(err) {
-        res.status(500).json({error: error.message});
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already Exists" });
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    res.status(200).json({ message: "User Registered Successfully!!!" });
+  } catch (err) {
+    res.status(500).json({ error: error.message });
+  }
 };
 
-export const login = async (req, res) => {
-    try {
-        const {email, password} = req.body;
-        const user = User.findOne({email});
-        if (user && user.compare(password, user.password)){
-            const token = jwt.sign({id: user._id}, process.env.JWT_SECRET_KEY, {expiresIn: process.env.JWT_EXPIRY})
-            res.json({
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                token
-            });
-        } else {
-            res.status(401).json({message: 'Invalid Email or Password!!!'})
-        }
-    } catch(err) {
-        res.status(500).json({error: error.message});
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = User.findOne({ email });
+    if (user && user.compare(password, user.password)) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET_KEY, {
+        expiresIn: process.env.JWT_EXPIRY,
+      });
+      res.json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        token,
+      });
+    } else {
+      res.status(401).json({ message: "Invalid Email or Password!!!" });
     }
+  } catch (err) {
+    res.status(500).json({ error: error.message });
+  }
 };
--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const cors = require('cors');
-const colors = require('colors');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const colors = require("colors");
+const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
-dotenv.config()
+dotenv.config();
 
 const app = express();
 
@@ -15,9 +16,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api/v1/auth", authRoutes);
+
 // test route
-app.get('/', (req, res) => {
-  res.send('MPFL API running');
+app.get("/", (req, res) => {
+  res.send("MPFL API running");
 });
 
 const PORT = process.env.PORT || 3000;
